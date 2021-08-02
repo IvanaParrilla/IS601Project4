af0b66e6335e28abb7c6eca2d5790408eb1c3f38
Ivana Parrilla <ip22@njit.edu>
2021-08-02 18:35:13 -0400

    Task: Edited Branch I

--- app/app.py
+++ app/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, make_response, request, jsonify
-from flaskext.mysql import MySQL
-from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
-mysql = MySQL(cursorclass=DictCursor)
+#mysql = MSQL (cursorclass=DictCursor)
 
-app.config['MYSQL_DATABASE_HOST'] = 'db'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'housingPrices'
-mysql.init_app(app)
+# app.config['MYSQL_DATABASE_HOST'] = 'db'
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+# app.config['MYSQL_DATABASE_PORT'] = 3306
+# app.config['MYSQL_DATABASE_DB'] = 'housingPrices'
+# mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
@@ -20,16 +18,14 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/table', methods=['GET'])
-def table():
-    if request.method != 'GET':
-        return make_response('Malformed request', 400)
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM zillow')
-    result = cursor.fetchall()
-    headers = {"Content-Type:": "application/json"}
-    return make_response(jsonify(result), 200, headers)
+# @app.route('/table', methods=['GET'])
+# def table():
+#     if request.method != 'GET':
+#         return make_response('Malformed request', 400)
+#     cursor = mysql.get_db().cursor()
+#     cursor.execute('SELECT * FROM zillow')
+#     result = cursor.fetchall()
+#     return render_template('table.html', houses=result)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
