213cf2239bc4fff4848fb3b7d5e96256e7febca7
Ivana Parrilla <ip22@njit.edu>
2021-08-02 23:31:55 -0400

    Task: Setting up Step III

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, make_response, request, jsonify
+from flask import Flask, render_template, make_response, request, jsonify, redirect, url_for
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+from forms import ListingForm
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
@@ -10,6 +11,7 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'housingPrices'
+app.config['SECRET_KEY'] = 'secret'
 mysql.init_app(app)
 
 
@@ -17,7 +19,8 @@ mysql.init_app(app)
 def home():
     if request.method != 'GET':
         return make_response('Malformed request', 400)
-    return render_template('home.html')
+    return render_template('home.html', title="Welcome Home", description="Your Resource For House Listings")
+
 
 @app.route('/table', methods=['GET'])
 def table():
@@ -28,6 +31,16 @@ def table():
     result = cursor.fetchall()
     headers = {"Content-Type:": "application/json"}
     return make_response(jsonify(result), 200, headers)
+
+
+@app.route('/add_listing', methods=['GET', 'POST'])
+def add_listing():
+    form = ListingForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('add.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
