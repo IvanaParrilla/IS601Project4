5321225f6004704a4e8d980ebd51a2f4a9c44237
Ivana Parrilla <ip22@njit.edu>
2021-08-03 10:23:39 -0400

    Task: Step V. Updated

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from pymysql.cursors import DictCursor
 from forms import ListingForm
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
 mysql = MySQL(cursorclass=DictCursor)
 
 app.config['MYSQL_DATABASE_HOST'] = 'db'
@@ -33,6 +35,14 @@ def table():
     return make_response(jsonify(result), 200, headers)
 
 
+@app.route('/zillow/<zipcode>', methods=['GET', 'POST'])
+def find_zipcode(zipcode):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM zillow WHERE zip = %s', zipcode)
+    result = cursor.fetchall()
+    return render_template('table.html', houses=result)
+
+
 @app.route('/add_listing', methods=['GET', 'POST'])
 def add_listing():
     form = ListingForm()
@@ -41,6 +51,5 @@ def add_listing():
     return render_template('add.html', form=form)
 
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
